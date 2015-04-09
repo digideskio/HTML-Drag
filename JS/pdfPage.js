5242c6010edbf88bf27cd757a4ae28a16e8ecf1e
dsloskyUSGS <dslosky@usgs.gov>
2015-04-09 21:47:41 +0000

    basic frame of the page, drag and drop kind of works, but not super well

---
+++ JS/pdfPage.js
@@ -0,0 +1,68 @@
+var main = function() {
+    
+    $('p').draggable({
+        appendTo: "body",
+        start: stopScroll,
+        stop: startScroll
+    });
+    
+    $('.pdf').droppable()
+    
+    $('.dropdown-toggle').click(function (event) {
+        event.stopPropagation();
+        $('.dropdown-toggle').dropdown();
+        
+    });
+    
+    $('.AmerDrop').click(function() {
+        var curSheet = $('.current');
+        var nextSheet = $('.Amer');
+        
+        if (curSheet!==nextSheet) {
+            curSheet.fadeOut(600).removeClass('current');
+            nextSheet.fadeIn(600).addClass('current');
+        }
+        
+    });
+    
+    $('.EuroDrop').click(function() {
+        var curSheet = $('.current');
+        var nextSheet = $('.Euro');
+        
+        if (curSheet!==nextSheet) {
+            curSheet.fadeOut(600).removeClass('current');
+            nextSheet.fadeIn(600).addClass('current');
+        }
+        
+    });
+    
+    $('.pdf').click(function() {
+        var curSheet = $('.current');
+        var nextSheet = curSheet.next();
+        
+        if (nextSheet.length === 0) {
+            nextSheet = $('.pdf').first();
+        };
+        
+        curSheet.fadeOut(600).removeClass('current');
+        nextSheet.fadeIn(600).addClass('current');
+        
+    });
+    
+};
+
+    function stopScroll() {
+
+        $('.scrollBox').css({
+            overflow: 'visible',
+        });
+    };
+
+    function startScroll() {
+        $('.scrollBox').css({
+            overflow: 'auto',
+        });
+    };
+
+$(document).ready(main);
+
